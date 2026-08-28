9030dced1878d5bb897d68f2748214f4393d7b66
jacksoncroque <jackson.crq@gmail.com>
2026-08-28 12:56:20 -0300

    Finalizado responsividade componente Products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -6,7 +6,7 @@ const Products = () => {
   return (
     <section className="bg-[rgb(251,246,232)]" id="products">
       {/*título*/}
-      <div className="mx-auto py-8 max-w-7xl">
+      <div className="mx-auto py-12 max-w-7xl px-4">
         <h1 className="font-anton flex flex-col my-4 text-[48px]">
           <span className="font-caveat text-[#E34935] text-4xl leading-6">
             o que a gente cria
@@ -15,7 +15,7 @@ const Products = () => {
         </h1>
 
         {/*produtos*/}
-        <div className="grid grid-cols-2 gap-8">
+        <div className="grid md:grid-cols-2 gap-8">
           {data.map((item) => (
             <ProductCard item={item} key={item.id} />
           ))}
